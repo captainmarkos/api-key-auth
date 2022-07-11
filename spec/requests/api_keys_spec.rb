f4873caa61e2b1d9a31c933ce673d6c8b0cafa08
Mark Brettin <mbrettin@thedyrt.com>
2022-07-10 22:36:02 -0400

    refactoring api_keys_spec.rb

--- spec/requests/api_keys_spec.rb
+++ spec/requests/api_keys_spec.rb
@@ -1,82 +1,81 @@
 require 'rails_helper'
 
 RSpec.describe 'ApiKeys', type: :request do
-
   let(:user) { create(:user, :with_api_keys) }
+  let(:headers) { { 'Authorization' => "#{auth_scheme}" } }
 
   describe 'GET /api-keys' do
-    context 'with no auth header' do
-      it 'not authorized' do
-        get '/api-keys', headers: {}
-        expect(response).to have_http_status(:unauthorized) # 401
-      end
-    end
-=begin
-    context 'with auth header' do
-      let(:headers) do
-        {
-          'Authorization' => "Bearer #{user.api_keys.first.token}"
-        }
+    context 'with bearer authentication' do
+      context 'when missing token' do
+        let(:auth_scheme) { 'Bearer' }
+
+        it 'not authorized' do
+          get '/api-keys', headers: headers
+          expect(response).to have_http_status(:unauthorized) # 401
+        end
       end
 
-      it 'list bearer API keys' do
-        get '/api-keys', headers: headers
+      context 'with valid token' do
+        let(:auth_scheme) { "Bearer #{user.api_keys.first.token}" }
 
-        expect(response).to have_http_status(:no_content) # 204
+        it 'list bearer API keys' do
+          get '/api-keys', headers: headers
+          expect(response).to have_http_status(:no_content) # 204
+        end
       end
     end
-=end
   end
-
 
   describe 'POST /api-keys' do
-    let(:headers) { { 'Authorization' => "Basic #{encoded}" } }
+    context 'with basic authentication' do
+      let(:auth_scheme) { "Basic #{encoded}" }
 
-    context 'with basic auth and bad user name' do
-      let(:encoded) do
-        Base64.encode64("#{user.email}-make-it-bad:#{user.password}")
+      context 'when bad user name' do
+        let(:encoded) { Base64.encode64("#{user.email}-fail:#{user.password}") }
+
+        it 'not authorized' do
+          post '/api-keys', headers: headers
+          expect(response).to have_http_status(:unauthorized) # 401
+        end
       end
 
-      it 'not authorized' do
-        post '/api-keys', headers: headers
-        expect(response).to have_http_status(:unauthorized) # 401
-      end
-    end
+      context 'when bad password' do
+        let(:encoded) { Base64.encode64("#{user.email}:#{user.password}-fail") }
 
-    context 'with basic auth and bad password' do
-      let(:encoded) do
-        Base64.encode64("#{user.email}:#{user.password}-make-it-bad")
+        it 'not authorized' do
+          post '/api-keys', headers: headers
+          expect(response).to have_http_status(:unauthorized) # 401
+        end
       end
 
-      it 'not authorized' do
-        post '/api-keys', headers: headers
-        expect(response).to have_http_status(:unauthorized) # 401
-      end
-    end
+      context 'when good credentials' do
+        let(:encoded) { Base64.encode64("#{user.email}:#{user.password}") }
 
-    context 'with basic auth and good credentials' do
-      let(:encoded) do
-        Base64.encode64("#{user.email}:#{user.password}")
-      end
-
-      it 'creates an ApiKey' do
-        post '/api-keys', headers: headers
-        expect(response.content_type).to eq('application/json; charset=utf-8')
-        expect(response).to have_http_status(:created)
+        it 'creates an ApiKey' do
+          post '/api-keys', headers: headers
+          expect(response.content_type).to eq('application/json; charset=utf-8')
+          expect(response).to have_http_status(:created)
+        end
       end
     end
   end
-
 =begin
 
-  context 'DELETE /api-keys' do
-    it 'creates an ApiKey' do
-      post '/api-keys', headers: headers
-      #post '/api-keys', params: { widget: {name: 'My Widget'} }, headers: headers
+  describe 'DELETE /api-keys' do
+    context '' do
+      let(:auth_scheme) { "Basic #{encoded}" }
 
-      expect(response.content_type).to eq('application/json; charset=utf-8')
-      expect(response).to have_http_status(:created)
+      context '' do
+        it 'destroys an ApiKey' do
+          post '/api-keys', headers: headers
+          #post '/api-keys', params: { api_key: {id: 1} }, headers: headers
+
+          expect(response.content_type).to eq('application/json; charset=utf-8')
+          expect(response).to have_http_status(:created)
+        end
+      end
     end
   end
+
 =end
 end
